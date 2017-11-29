c726fcdba76b6d6edf84ddb417524189e74e1f6a
Jesper <R3wE@hotmail.com>
2017-11-29 23:03:13 +0100

    Again

---
+++ Bodega.dev/Bodega.dev/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Bodega.dev.MvcApplication" Language="C#" %>
---
+++ Bodega.dev/Bodega.dev/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bodega.dev.Startup))]
+namespace Bodega.dev
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
