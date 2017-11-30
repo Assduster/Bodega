67673b333986809d0ef6db848af59d3e93bdc82a
XSyian <Xsyian@gmail.com>
2017-11-30 20:41:32 +0100

    Galleri

--- Bodega.dev/Bodega.dev/Scripts/script.js
+++ Bodega.dev/Bodega.dev/Scripts/script.js
@@ -1,27 +1,28 @@
 ﻿$(document).ready(function () {
     console.log("Galery sidan körs");
 });
-$("#showInformationtext").hide();
+$("#showInformationtext1").hide();
 $("#showInformationtext2").hide();
 $("#showInformationtext3").hide();
+$("#showInformationtext4").hide();
+$("#showInformationtext5").hide();
+$("#showInformationtext6").hide();
 
 $("#bild1").click(function () {
+    $("#imagediv2").fadeToggle();
+    $("#showInformationtext1").fadeToggle();
     $("#bild2").fadeToggle();
     $("#bild3").fadeToggle();
-    $("#showInformationtext").show();
 });
 $("#bild2").click(function () {
-
+    $("#imagediv2").fadeToggle();
+    $("#showInformationtext2").fadeToggle();
+    $("#bild1").fadeToggle();
+    $("#bild3").fadeToggle();
 });
 $("#bild3").click(function () {
-
-});
-$("#bild4").click(function () {
-
-});
-$("#bild5").click(function () {
-
-});
-$("#bild6").click(function () {
-
+    $("#imagediv2").fadeToggle();
+    $("#showInformationtext3").fadeToggle();
+    $("#bild2").fadeToggle();
+    $("#bild1").fadeToggle();
 });
