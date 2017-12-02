1fbd545d861774e957e94c15c053dbec300ec50d
XSyian <Xsyian@gmail.com>
2017-12-02 20:12:19 +0100

    Kontaka oss overallt

--- Bodega.dev/Bodega.dev/Scripts/script.js
+++ Bodega.dev/Bodega.dev/Scripts/script.js
@@ -10,6 +10,11 @@
         $("#HomePage").hide();
        
     }
+
+    var panels = $('.vote-results');
+    var panelsButton = $('.dropdown-results');
+    panels.hide();
+
     getnewsdataapi();
     $(".navigation").click(function () {
         var navDestination = this.href.substr(this.href.indexOf("#") + 1);
@@ -38,10 +43,29 @@
             $("#HomePage").empty();
 
             for (var i = 0; i < data.length; i++) {
-                var html = "<div class='well'><div class='media'><a class='pull-left' href='#'><img class='media- object' src='#'></a><div class='media-body'><h4 class='media- heading'>" + data[i].Title + "</h4><p class='text- right'>" + data[i].Text + "</p><ul class='list-inline list-unstyled'><li><span><i class='glyphicon glyphicon-calendar'></i>" + data[i].Published + "</span></li></ul></div></div></div>";
+                var html = "<div class='well'><div class='media'><a class='pull-left' href='#'><img class='media- object' src='#'></a><div class='media-body'><h3 class='media- heading' style='font-family:Fjord One, serif; font-weight:bold;'>" + data[i].Title + "</h3><p class='text- right' style='font-family:Fjord One, serif; font-size:17px; font-weight:500;'>" + data[i].Text + "</p><ul class='list-inline list-unstyled'><li><span><i class='glyphicon glyphicon-calendar'></i>" + data[i].Published + "</span></li></ul></div></div></div>";
             $("#HomePage").append(html);
              
             }
         });
     }
+
+    panelsButton.click(function () {
+        //get data-for attribute
+        var dataFor = $(this).attr('data-for');
+        var idFor = $(dataFor);
+
+        //current button
+        var currentButton = $(this);
+        idFor.slideToggle(400, function () {
+            //Completed slidetoggle
+            if (idFor.is(':visible')) {
+                currentButton.html('Hide Results');
+            }
+            else {
+                currentButton.html('View Results');
+            }
+        })
+    });
 });
+
