ddb9a3e9dbbf4fbf106cbff296fb8215f00afede
kirreee <chickens@live.se>
2017-12-06 02:12:38 +0100

    GG
    GG

--- Bodega.dev/Bodega.dev/Models/News.cs
+++ Bodega.dev/Bodega.dev/Models/News.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -11,5 +13,19 @@ namespace Bodega.dev.Models
         public string Title { get; set; }
         public string Text { get; set; }
         public DateTime Published { get; set; }
+        public FileUpload Image { get; set; }
+        public int ImageId { get; set; }
+
+
+    }
+    public class FileUpload
+    {
+       
+       
+        [Key]
+        public int Id{get;set;}
+        public string imagename {get;set;}
+        public byte[] imagedata{get; set;
+        } 
     }
 }
